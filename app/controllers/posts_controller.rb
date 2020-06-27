85f88d94d49a6dea723bf5cd9f5d1f142897f66d
Takuma Tajikawa <takumatajikawa@TakumanoMacBook-Pro.local>
2020-06-27 19:30:52 +0900

    バリデーションエラー時に投稿ページをレンダリングするように修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,9 +14,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create!(post_params)
+    @post = Post.new(post_params)
     if @post.save
-      redirect_to post, notice: "投稿しました"
+      redirect_to @post, notice: "投稿しました"
     else
       flash.now[:alert] = "投稿に失敗しました"
       render :new
